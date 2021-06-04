d9d4b085e16b1b40e86835c3ec15f253cdb4eb6b
CReid <creid9498@gmail.com>
2021-06-04 15:34:51 +0100

    ruby added, fixed error in ModItems.java

--- src/main/java/com/sooty/mymod/Mymod.java
+++ src/main/java/com/sooty/mymod/Mymod.java
@@ -1,11 +1,15 @@
 package com.sooty.mymod;
 
+import com.sooty.mymod.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class Mymod implements ModInitializer {
 
+    public static final String MOD_ID = "mymod";
+
     @Override
     public void onInitialize() {
+        ModItems.registerItems();
 
     }
 }
